079c2ce854174c925c182ee47873a19317acc53c
Mert Acar <mert25acar@gmail.com>
2024-10-08 10:39:07 +0300

    fixed the israeli-palestinian conflict

--- nvim/lua/macar/remap.lua
+++ nvim/lua/macar/remap.lua
@@ -11,26 +11,33 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]], {desc= "paste without replace"})
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "paste without replace" })
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], {desc = "Copy into system clipboard"})
-vim.keymap.set("n", "<leader>Y", [["+Y]], {desc = "Copy into system clipboard"})
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy into system clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Copy into system clipboard" })
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]], {desc = "Delete without yank"})
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without yank" })
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<leader>c", function () vim.lsp.buf.format({ async = true}) end, {desc = "Format buffer"})
+vim.keymap.set("n", "<leader>c", function()
+	vim.lsp.buf.format({ async = true })
+end, { desc = "Format buffer" })
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz", {desc = "Next quick-fix"})
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", {desc = "Prev quick-fix"})
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz", { desc = "Next quick-fix" })
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", { desc = "Prev quick-fix" })
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], {desc = "Search and replace the word under the cursor"})
+vim.keymap.set(
+	"n",
+	"<leader>s",
+	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+	{ desc = "Search and replace the word under the cursor" }
+)
 
 -- Stay in indent mode
 vim.keymap.set("v", "<", "<gv^")
